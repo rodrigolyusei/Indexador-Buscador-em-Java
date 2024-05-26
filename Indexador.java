1b55b4e1750dbc6a2198e5f0e2f31354e1c4edc5
JolidPnake <rodrigo0607suguimoto@gmail.com>
2024-05-26 16:02:06 -0300

    add: codigo para ler arquivo

--- Indexador.java
+++
@@ -1,5 +0,0 @@
-public class Indexador {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ src/Indexador.java
@@ -0,0 +1,12 @@
+package src;
+
+//import src.ManipulaArquivo;
+
+public class Indexador {
+    public static void main(String[] args) {
+
+        ManipulaArquivo manipulaArquivo = new ManipulaArquivo();
+        manipulaArquivo.ler("./data/teste.txt");
+        
+    }
+}
