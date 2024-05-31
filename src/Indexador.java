9e16048d48afc93b07411f2de5197ad3e380b8ec
JolidPnake <rodrigo0607suguimoto@gmail.com>
2024-05-31 02:58:30 -0300

    feat: mudar de switch para if

--- src/Indexador.java
+++ src/Indexador.java
@@ -1,21 +1,17 @@
 package src;
 
-//import src.ManipulaArquivo;
-
 public class Indexador {
     public static void main(String[] args) {
         ManipulaArquivo manipulaArquivo = new ManipulaArquivo();
 
-        switch(args.length){
-            case 1:
-                manipulaArquivo.ler(args[0]);
-                break;
-            case 2:
-                manipulaArquivo.ler(args[0]);
-                break;
-            default:
-                System.out.println("Argumento inválido");
+        if(args.length==1){
+            manipulaArquivo.ler(args[0]);
+            return;
         }
-        
+        if(args.length==2){
+            manipulaArquivo.ler(args[0]);
+            return;
+        }
+        System.out.println("Argumento inválido");
     }
 }
